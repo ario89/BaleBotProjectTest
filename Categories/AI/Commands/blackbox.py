a140f71a9792f59b7c39e65956fadf270d64d800
Ario Shakeri <itsario900@gmail.com>
2024-09-14 20:39:14 +0330

    Added Blackbox AI Command

---
+++ Categories/AI/Commands/blackbox.py
@@ -0,0 +1,18 @@
+from bale import Message
+from Categories.AI.AI import AICommand, backButton
+from codern import api
+
+@AICommand("blackbox", "📦 BlackBox AI", 1)
+async def blackbox(message:Message, query:Message=False, *args):
+    if not query:
+        await message.reply("❓ What Do You Want Help With? ")
+        return "query"
+    
+    msg = await query.reply("🪄 Typing...")
+    
+    try:
+        result:str = api.Ai_black_box(query.content)
+        await msg.edit(result.removeprefix("v=undefined"))
+    except Exception as e:
+        await msg.edit("❌ Error")
+        print(e)
